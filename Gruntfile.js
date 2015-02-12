3aaf6fed729cb1b531315b78be8ee5ab284b15cd
Dylan O'Carroll <dylancocarroll@gmail.com>
2015-02-11 22:13:49 -0800

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,4 @@
 module.exports = function() {
   this.loadTasks("build/tasks");
-  this.registerTask("gnar", ["clean", "processhtml", "copy", "dustjs:compile", "cssmin", "styles"]);
   return this.registerTask("default", ["libsass", "cssmin", "clean", "dustjs:compile", "connect", "watch"]);
 };
